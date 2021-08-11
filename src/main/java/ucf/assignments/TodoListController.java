4271a9f9b93c6dbfa6d4fe12f996525df18cf371
kmullens1 <kmullens1@knights.ucf.edu>
2021-08-11 01:48:44 -0400

    Updates: Can now run and SceneBuilder stuff loads TestCases have been set up for future reference UML Diagram has been updated Many classes have been created

---
+++ src/main/java/ucf/assignments/TodoListController.java
@@ -0,0 +1,47 @@
+package ucf.assignments;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+//FIXME do I need the above line? It was in the professor's calculator code
+
+public class TodoListController {
+    @FXML
+    private DatePicker chooseDueDate;
+
+    @FXML
+    private TextField taskDescriptionTextBox;
+
+    @FXML
+    public void saveButtonPressed(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void loadButtonPressed(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void displayCompleteBoxChecked(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void displayIncompleteBoxChecked(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void addTaskButtonPressed(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void deleteTaskButtonPressed(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void createListButtonPressed(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void helpButtonPressed(ActionEvent actionEvent) {
+    }
+}
---
+++ src/test/java/ucf/assignments/TodoListControllerTest.java
@@ -0,0 +1,43 @@
+package ucf.assignments;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TodoListControllerTest {
+
+    //Professor Hollander said I did not have to do tests for Assignments 4 and 5 due to time constraints...
+    //I may do some tests anyways. We shall see...
+
+    @Test
+    void saveButtonPressed() {
+    }
+
+    @Test
+    void loadButtonPressed() {
+    }
+
+    @Test
+    void displayCompleteBoxChecked() {
+    }
+
+    @Test
+    void displayIncompleteBoxChecked() {
+    }
+
+    @Test
+    void addTaskButtonPressed() {
+    }
+
+    @Test
+    void deleteTaskButtonPressed() {
+    }
+
+    @Test
+    void createListButtonPressed() {
+    }
+
+    @Test
+    void helpButtonPressed() {
+    }
+}
