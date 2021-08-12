6c19f948ccc91f6ae3e1919fa4e2a56022a33b36
kmullens1 <kmullens1@knights.ucf.edu>
2021-08-12 01:44:07 -0400

    Pseudo Code Updated Some

--- src/main/java/ucf/assignments/TodoListController.java
+++ src/main/java/ucf/assignments/TodoListController.java
@@ -1,3 +1,7 @@
+/*
+ * UCF COP3330 Summer 2021 Assignment 4 Solution
+ * Copyright 2021 Keri Mullens
+ */
 package ucf.assignments;
 
 import javafx.event.ActionEvent;
@@ -14,33 +18,62 @@ public class TodoListController {
 
     @FXML
     public void saveButtonPressed(ActionEvent actionEvent) {
+        //file chooser??? -- if so, another .fxml page is needed due to file chooser pop-up
+        //option to name file?
+        //send data to save location
     }
 
     @FXML
     public void loadButtonPressed(ActionEvent actionEvent) {
+        //Ask name of file to load?? - I don't think there are multiple files to choose from... idk
+        //access saved memory from save location
+        //print saved memory
     }
 
     @FXML
     public void displayCompleteBoxChecked(ActionEvent actionEvent) {
+        //determine if each item is complete
+        //if so print item to screen
+        //OR
+        //Have a function that sorts actions as they are completed into a "complete list"
+        //print complete list
     }
 
     @FXML
     public void displayIncompleteBoxChecked(ActionEvent actionEvent) {
+        //determine if each item is incomplete
+        //if so print item to screen
+        //OR
+        //Have a function that initially adds new items to another "incomplete list"
+            //Once item is complete it is moved to complete list
+        //print incomplete list
     }
 
     @FXML
     public void addTaskButtonPressed(ActionEvent actionEvent) {
+        //new row is added (printed out?)
+        //Ask for task description
+        //Ask for due date (date picker??)
+        //print description and due date
     }
 
     @FXML
     public void deleteTaskButtonPressed(ActionEvent actionEvent) {
+        //ask which row to delete?
+            //OR delete row that is selected...
+        //select that row and eliminate it
+            //Make sure incomplete and complete lists are also updated
     }
 
     @FXML
     public void createListButtonPressed(ActionEvent actionEvent) {
+        //old list is reset
+        //1st task and due date are requested?? or is initial new list blank?
     }
 
     @FXML
     public void helpButtonPressed(ActionEvent actionEvent) {
+        //pop up window - new .fxml file??
+        //dedication to Rey
     }
 }
