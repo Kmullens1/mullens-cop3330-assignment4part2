d742ca2e03d0ddb20833141dff16d3de9af014ed
kmullens1 <kmullens1@knights.ucf.edu>
2021-10-25 02:49:09 -0400

    IT FINALLY COMPILES!!!! :D

--- src/main/java/ucf/assignments/TodoList.java
+++ src/main/java/ucf/assignments/TodoList.java
@@ -4,33 +4,29 @@
  */
 
 package ucf.assignments;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class TodoList extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("TodoList.fxml"));
+
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
+
+        stage.setTitle("JavaFX and Gradle");
+        stage.setScene(scene);
+        stage.show();
+    }
 
     public static void main(String[] args) {
         launch(args);
     }
 
-    //Start Method
-    @Override
-    public void start(Stage primaryStage) {
-        try {
-            Parent root = FXMLLoader.load(getClass().getResource("TodoList.fxml"));
-
-            Scene scene = new Scene(root);
-
-            primaryStage.setScene(scene);
-            primaryStage.setTitle("TodoList");
-            primaryStage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 }
